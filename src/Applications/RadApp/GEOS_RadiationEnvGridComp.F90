53b286e5052b65ceabfbf0781f0733377e4e0ade
Matthew Thompson <matthew.thompson@nasa.gov>
2022-03-31 10:43:34 -0400

    Try the character array fix again

--- src/Applications/RadApp/GEOS_RadiationEnvGridComp.F90
+++ src/Applications/RadApp/GEOS_RadiationEnvGridComp.F90
@@ -367,7 +367,7 @@ contains
    real, pointer                       :: aeroInternal(:,:,:) => null()
    real                                :: tempor, plt
    integer, parameter :: numAEROs = 15
-   character(len=ESMF_MAXSTR) :: aeroNames(numAEROs) = [character(len=8) :: &
+   character(len=ESMF_MAXSTR) :: aeroNames(numAEROs) = [character(len=ESMF_MAXSTR) :: &
                                                          "du001", "du002", "du003", "du004", &
                                                          "du005", "ss001", "ss002", "ss003", &
                                                          "ss004", "ss005", "SO4"  , "BCphobic", &
