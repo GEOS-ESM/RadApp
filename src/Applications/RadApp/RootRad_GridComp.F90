c90b77732187682b2b02fd45d270cc5f74e02026
Peter Norris <peter.norris@nasa.gov>
2023-01-13 13:43:02 -0500

    pmn: initial cleanup

--- src/Applications/RadApp/RootRad_GridComp.F90
+++ src/Applications/RadApp/RootRad_GridComp.F90
@@ -4,14 +4,14 @@
 !-------------------------------------------------------------------------
 !BOP
 !
-! !MODULE: RootRad_GridComp - Implements Interface to a minimalistic MAPL GC that
-!                   serves as a parent of radiation and pchem
+! !MODULE: RootRad_GridComp - Implements Interface to a minimalistic MAPL
+!             GC that serves as a parent of radiation and pchem
 !
 ! !INTERFACE:
 !
 #include "MAPL_Generic.h"
 
-   MODULE RootRad_GridCompMod
+MODULE RootRad_GridCompMod
 !
 ! !USES:
 !
@@ -19,8 +19,8 @@
    USE MAPL_Mod
 
    USE GEOS_RadiationEnvGridCompMod, only : RadiationEnvSetServices => SetServices
-   USE GEOS_RadiationGridCompMod, only : RadiationSetServices => SetServices
-   use GEOS_ChemGridCompMod,       only : AChemSetServices     => SetServices
+   USE GEOS_RadiationGridCompMod,    only : RadiationSetServices    => SetServices
+   use GEOS_ChemGridCompMod,         only : AChemSetServices        => SetServices
 
    IMPLICIT NONE
    PRIVATE
@@ -35,165 +35,146 @@
 
 contains
 
+!=============================================================================
 !BOP
 
 ! !IROUTINE: SetServices -- Sets ESMF services for this component
 
 ! !INTERFACE:
 
-    subroutine SetServices ( GC, RC )
+   subroutine SetServices (GC, RC)
 
 ! !ARGUMENTS:
 
-    type(ESMF_GridComp), intent(INOUT) :: GC  ! gridded component
-    integer,             intent(  OUT) :: RC  ! return code
+      type(ESMF_GridComp), intent(INOUT) :: GC  ! gridded component
+      integer,             intent(  OUT) :: RC  ! return code
 
-! !DESCRIPTION:  The SetServices registers the Radiation component
+! !DESCRIPTION: The SetServices registers the Radiation component
 
 !EOP
-
-!=============================================================================
-!
-! ErrLog Variables
-
-    character(len=ESMF_MAXSTR)              :: IAm
-    integer                                 :: STATUS
-    character(len=ESMF_MAXSTR)              :: COMP_NAME
-
-    type(ESMF_Config)          :: cf
-    integer                    :: aeroProvider
-    CHARACTER(LEN=ESMF_MAXSTR) :: providerName, ratsName, RadCouple
-
 !=============================================================================
 
-! Begin...
+      character(len=ESMF_MAXSTR) :: IAm
+      integer                    :: STATUS
+      character(len=ESMF_MAXSTR) :: COMP_NAME
+
+      type(ESMF_Config)          :: cf
+      integer                    :: aeroProvider
+      CHARACTER(LEN=ESMF_MAXSTR) :: providerName, ratsName, RadCouple
+
+!=============================================================================
 
 ! Get my name and set-up traceback handle
 ! ---------------------------------------
 
-    Iam = 'SetServices'
-    call ESMF_GridCompGet( GC, NAME=COMP_NAME, CONFIG=CF, RC=STATUS )
-    VERIFY_(STATUS)
-    Iam = trim(COMP_NAME) // "::" // Iam
+      Iam = 'SetServices'
+      call ESMF_GridCompGet (GC, NAME=COMP_NAME, CONFIG=CF, _RC)
+      Iam = trim(COMP_NAME) // "::" // Iam
 
-!   Set the Initialize, Run, Finalize entry points
-!   ----------------------------------------------
-    call MAPL_GridCompSetEntryPoint ( GC, ESMF_METHOD_INITIALIZE,  Initialize_, RC=status)
-    VERIFY_(STATUS)
-    call MAPL_GridCompSetEntryPoint ( GC, ESMF_METHOD_RUN,   Run_, RC=status)
-    VERIFY_(STATUS)
+! Set the Initialize, Run, Finalize entry points
+! ----------------------------------------------
+      call MAPL_GridCompSetEntryPoint (GC, ESMF_METHOD_RUN, Run_, _RC)
 
-!   Register children
-!   -----------------
-    RADENV = MAPL_AddChild(GC, NAME='RADENV', SS=RadiationEnvSetServices, RC=STATUS)
-    VERIFY_(STATUS)
-    RAD = MAPL_AddChild(GC, NAME='RADIATION', SS=RadiationSetServices, RC=STATUS)
-    VERIFY_(STATUS)
+! Register children
+! -----------------
+      RADENV = MAPL_AddChild (GC, NAME='RADENV',    SS=RadiationEnvSetServices, _RC)
+      RAD    = MAPL_AddChild (GC, NAME='RADIATION', SS=RadiationSetServices,    _RC)
 
-!   Determine AERO and RATS provider
-!   --------------------------------
-    call ESMF_ConfigGetAttribute(CF, ratsName, Default="PCHEM", &
-                               Label="RATS_PROVIDER:", __RC__ )
-    if (trim(ratsName) == "PCHEM") then
-       chem = MAPL_AddChild(GC, NAME=    'CHEMISTRY', SS=AChemSetServices, RC=STATUS)
-       VERIFY_(STATUS)
-    end if
+! Determine AERO and RATS provider
+! --------------------------------
+      call ESMF_ConfigGetAttribute (CF, &
+         ratsName, Default="PCHEM", Label="RATS_PROVIDER:", _RC)
+      if (trim(ratsName) == "PCHEM") then
+         CHEM = MAPL_AddChild (GC, NAME='CHEMISTRY', SS=AChemSetServices, _RC)
+      end if
 
-!   Imports of Radiation calculated in RadEnv not to come thru ExtData
-!   ------------------------------------------------------------------
-    call MAPL_AddConnectivity ( GC,                               &
-        SHORT_NAME  = [character(len=4):: 'PREF','RI','RL'],      &
-        DST_ID      =  RAD,                                       &
-        SRC_ID      =  RADENV,                                    &
-                                                        RC=STATUS  )
-    VERIFY_(STATUS)
+! Imports of Radiation calculated in RadEnv not to come thru ExtData
+! ------------------------------------------------------------------
+      call MAPL_AddConnectivity (GC, &
+         SHORT_NAME  = [character(len=4):: 'PREF','RI','RL'], &
+         DST_ID      =  RAD,         &
+         SRC_ID      =  RADENV,   _RC)
 
-    call ESMF_ConfigGetAttribute(CF, providerName, Default="GOCART.data", &
-                               Label="AERO_PROVIDER:", __RC__ )
+      call ESMF_ConfigGetAttribute (CF, &
+         providerName, Label="AERO_PROVIDER:", Default="GOCART.data", _RC)
 
 !   if provider of aerobundle is pchem then we must terminate imports for its contents
-!   so radenv doesn't expect them
-    if (providerName == "GOCART.data" .or. providerName == "GOCART") then
-       ASSERT_(trim(ratsName) == "PCHEM")
-       aeroProvider = CHEM
-       call MAPL_TerminateImport(GC, &
+!?   so radenv doesn't expect them
+      if (providerName == "GOCART.data" .or. providerName == "GOCART") then
+         ASSERT_(trim(ratsName) == "PCHEM")
+         aeroProvider = CHEM
+         call MAPL_TerminateImport (GC, &
             SHORT_NAME = [character(len=9):: 'FRLAND','FRLANDICE','FROCEAN','FRACI'],  &
             CHILD = aeroProvider, &
             RC = STATUS)
-    else
-       ASSERT_(.false.)
-    end if
+      else
+         ASSERT_(.false.)
+      end if
 
-
-     call MAPL_AddConnectivity ( GC,                               &
-         SHORT_NAME  = (/'AERO'/),                                 &
-         DST_ID      =  RAD,                                       &
-         SRC_ID      =  aeroProvider,                              &
-                                                        RC=STATUS  )
-     VERIFY_(STATUS)
+      call MAPL_AddConnectivity (GC,   &
+         SHORT_NAME = (/'AERO'/),      &
+         DST_ID     = RAD,             &
+         SRC_ID     = aeroProvider, _RC)
 
 !    for now pchem is the rats provider, no alternative
 !    --------------------------------------------------
-     if (trim(ratsName) == "PCHEM") then
-        CALL MAPL_AddConnectivity( GC, &
-            SHORT_NAME  = (/'OX    ','O3    ','CH4   ','N2O   ', &
-                            'CFC11 ','CFC12 ','HCFC22'       /), &
-                           DST_ID=RAD, SRC_ID=CHEM, RC=STATUS    )
-        VERIFY_(STATUS)
-     end if
+      if (trim(ratsName) == "PCHEM") then
+         call MAPL_AddConnectivity (GC, &
+            SHORT_NAME = [character(len=6):: 'OX','O3','CH4','N2O','CFC11','CFC12','HCFC22'], &
+            DST_ID     = RAD,           &
+            SRC_ID     = CHEM,       _RC)
+      end if
 
 !    Now have alternative Radiation coupling
-     call ESMF_ConfigGetAttribute(CF, RadCouple, Default="USE_MODEL_RADCOUPLE_EXPORTS", &
-                      Label="RADCOUPLE_METHOD:", __RC__ )
-     if (trim(RadCouple) == "USE_MODEL_RADCOUPLE_EXPORTS") then
-        if (MAPL_AM_I_ROOT()) then
-           write(*,*)"Using default radiation coupling, QV, QL, QI, FCLD will be provided by ExtData"
-        end if
-     else if (trim(RadCouple) == "NEW_METHOD_1") then
-        if (MAPL_AM_I_ROOT()) then
-           write(*,*)"Using new_method_1, QV,QL, QI, FCLD will be produced by RadEnv from Q,QILS,QICN, QLLS,QLCN,CLLS, and CLCN"
-        end if
-        call MAPL_AddConnectivity(GC, &
-             SHORT_NAME = (/'QL  ','QI  ','FCLD','QV  '/), &
-             DST_ID=RAD, SRC_ID=RADENV, RC=STATUS)
-        VERIFY_(STATUS)
-     end if
+      call ESMF_ConfigGetAttribute (CF, &
+         RadCouple, Default="USE_MODEL_RADCOUPLE_EXPORTS", Label="RADCOUPLE_METHOD:", _RC)
+      if (trim(RadCouple) == "USE_MODEL_RADCOUPLE_EXPORTS") then
+         if (MAPL_AM_I_ROOT()) then
+            write(*,*) "Using default radiation coupling,", &
+               "QV, QL, QI, FCLD will be provided by ExtData"
+         end if
+      else if (trim(RadCouple) == "NEW_METHOD_1") then
+         if (MAPL_AM_I_ROOT()) then
+            write(*,*) "Using new_method_1, QV,QL, QI, FCLD will be produced by RadEnv", &
+               "from Q,QILS,QICN, QLLS,QLCN,CLLS, and CLCN"
+         end if
+         call MAPL_AddConnectivity (GC, &
+            SHORT_NAME = [character(len=4):: 'QL','QI','FCLD','QV'], &
+            DST_ID     = RAD,           &
+            SRC_ID     = RADENV,     _RC)
+      end if
 
+! Generic Set Services
+! --------------------
+      call MAPL_GenericSetServices (GC, _RC)
 
-!   Generic Set Services
-!   --------------------
-    call MAPL_GenericSetServices ( GC, rc=status)
-    VERIFY_(STATUS)
+      RETURN_(ESMF_SUCCESS)
 
-    RETURN_(ESMF_SUCCESS)
+   end subroutine SetServices
 
-    end subroutine SetServices
-
+!=============================================================================
 !BOP
-!
-! !IROUTINE:  Initialize_ --- Initialize RUT
-!
+
+! !IROUTINE: Run_
+
 ! !INTERFACE:
-!
 
-   SUBROUTINE Initialize_ ( GC, IMPORT, EXPORT, CLOCK, rc )
+   subroutine Run_ (GC, IMPORT, EXPORT, CLOCK, RC)
 
 ! !USES:
 
-   implicit NONE
+     implicit NONE
 
 ! !INPUT PARAMETERS:
 
-   type(ESMF_Clock),  intent(inout) :: CLOCK     ! The clock
+      type (ESMF_Clock),    intent(INOUT) :: CLOCK   ! The clock
 
 ! !OUTPUT PARAMETERS:
 
-   type(ESMF_GridComp), intent(inout) :: GC      ! Grid Component
-   type(ESMF_State), intent(inout) :: IMPORT     ! Import State
-   type(ESMF_State), intent(inout) :: EXPORT     ! Export State
-   integer, intent(out)            :: rc         ! Error return code:
-                                                 !  0 - all is well
-                                                 !  1 -
+      type (ESMF_GridComp), intent(INOUT) :: GC      ! Grid Component
+      type (ESMF_State),    intent(INOUT) :: IMPORT  ! Import State
+      type (ESMF_State),    intent(INOUT) :: EXPORT  ! Export State
+      integer,              intent(  OUT) :: RC      ! Return code
 
 ! !DESCRIPTION: This is a simple ESMF wrapper.
 !
@@ -202,145 +183,72 @@ contains
 !  29 Sep 2011  Anton Darmenov   Cloned from the ExtData GC code
 !
 !EOP
-!-------------------------------------------------------------------------
+!=============================================================================
 
-
-   type(ESMF_Config)           :: CF          ! Universal Config
-   character(len=ESMF_MAXSTR)  :: Iam
-   integer                     :: status
-   character(len=ESMF_MAXSTR)  :: comp_name
-   integer :: im_world,jm_world,nx,ny,comm
-   type(MAPL_MetaComp),pointer :: MAPL
-   type(ESMF_VM) :: vm
-
-!  Get my name and set-up traceback handle
-!  ---------------------------------------
-   Iam = "Initialize_"
-   call ESMF_GridCompGet( GC, name=comp_name, config=CF, vm=vm, rc=status )
-   VERIFY_(STATUS)
-   call ESMF_VMGet(vm,mpiCommunicator=comm,rc=status)
-   VERIFY_(status)
-   call MAPL_GetObjectFromGC(GC,mapl,rc=status)
-   VERIFY_(status)
-   Iam = trim(comp_name) // '::' // trim(Iam)
-
-!  Create grid for this GC
-!  ------------------------
-   call MAPL_GridCreate  (GC, RC=status )
-   VERIFY_(STATUS)
-   call MAPL_GetResource(MAPL,im_world,label='IM:',rc=status)
-   VERIFY_(STATUS)
-   call MAPL_GetResource(MAPL,jm_world,label='JM:',rc=status)
-   VERIFY_(STATUS)
-   call MAPL_GetResource(MAPL,nx,label='NX:',rc=status)
-   VERIFY_(STATUS)
-   call MAPL_GetResource(MAPL,ny,label='NY:',rc=status)
-   VERIFY_(STATUS)
-
-!  Initialize MAPL Generic
-!  -----------------------
-   call MAPL_GenericInitialize ( GC, IMPORT, EXPORT, clock, RC=status)
-   VERIFY_(STATUS)
-
-!  All done
-!  --------
-   RETURN_(ESMF_SUCCESS)
-
-   END SUBROUTINE Initialize_
-
-   SUBROUTINE Run_ ( GC, IMPORT, EXPORT, CLOCK, rc )
-
-! !USES:
-
-  implicit NONE
-
-! !INPUT PARAMETERS:
-
-   type(ESMF_Clock),  intent(inout) :: CLOCK     ! The clock
-
-! !OUTPUT PARAMETERS:
-
-   type(ESMF_GridComp), intent(inout)  :: GC     ! Grid Component
-   type(ESMF_State), intent(inout) :: IMPORT     ! Import State
-   type(ESMF_State), intent(inout) :: EXPORT     ! Export State
-   integer, intent(out) ::  rc                   ! Error return code:
-                                                 !  0 - all is well
-                                                 !  1 -
-
-! !DESCRIPTION: This is a simple ESMF wrapper.
-!
-! !REVISION HISTORY:
-!
-!  29 Sep 2011  Anton Darmenov   Cloned from the ExtData GC code
-!
-!EOP
-!-------------------------------------------------------------------------
-
-   character(len=ESMF_MAXSTR)    :: Iam
-   integer                       :: STATUS
-   character(len=ESMF_MAXSTR)    :: comp_name
+      character(len=ESMF_MAXSTR)    :: Iam
+      integer                       :: STATUS
+      character(len=ESMF_MAXSTR)    :: comp_name
 
 ! Local derived type aliases
 
-   type (MAPL_MetaComp),      pointer  :: STATE
-   type (ESMF_GridComp),      pointer  :: GCS(:)
-   type (ESMF_State),         pointer  :: GIM(:)
-   type (ESMF_State),         pointer  :: GEX(:)
-   type (ESMF_State)                   :: INTERNAL
-   type (ESMF_Config)                  :: CF
-   character(len=ESMF_MAXSTR),pointer  :: GCNames(:)
-   integer                             :: I
-   integer                             :: IM, JM, LM
-   integer                             :: iphase
+      type (MAPL_MetaComp),      pointer  :: STATE
+      type (ESMF_GridComp),      pointer  :: GCS(:)
+      type (ESMF_State),         pointer  :: GIM(:)
+      type (ESMF_State),         pointer  :: GEX(:)
+      character(len=ESMF_MAXSTR),pointer  :: GCNames(:)
+      integer                             :: I
+
+!=============================================================================
 
 !  Get my name and set-up traceback handle
 !  ---------------------------------------
-   Iam = "Run_"
-   call ESMF_GridCompGet( GC, name=comp_name, rc=status )
-   VERIFY_(STATUS)
-   Iam = trim(comp_name) // '::' // trim(Iam)
+      Iam = "Run_"
+      call ESMF_GridCompGet( GC, name=comp_name, _RC)
+      Iam = trim(comp_name) // '::' // trim(Iam)
 
-! Get my internal MAPL_Generic state
+! Get my MAPL_Generic state
 !-----------------------------------
 
-   call MAPL_GetObjectFromGC ( GC, STATE, RC=STATUS)
-   VERIFY_(STATUS)
+      call MAPL_GetObjectFromGC (GC, STATE, _RC)
 
 ! Get the children`s states from the generic state
 !-------------------------------------------------
 
-   call MAPL_Get ( STATE,   &
-       GCS=GCS, GIM=GIM, GEX=GEX,       &
-       IM = IM, JM = JM, LM = LM,       &
-       GCNames = GCNames,               &
-       INTERNAL_ESMF_STATE = INTERNAL,  &
-                               RC=STATUS )
-   VERIFY_(STATUS)
+      call MAPL_Get (STATE,             &
+          GCS=GCS, GIM=GIM, GEX=GEX,    &
+          GCNames=GCNames,           _RC)
 
-   I=CHEM
+! Run the Radiation Code and its supporters
+! -----------------------------------------
 
-   iphase = 1
-   call ESMF_GridCompRun (GCS(I), importState=GIM(I), exportState=GEX(I), clock=CLOCK, phase=iphase, userRC=STATUS ); VERIFY_(STATUS)
-   iphase = 2
-   call ESMF_GridCompRun (GCS(I), importState=GIM(I), exportState=GEX(I), clock=CLOCK, phase=iphase, userRC=STATUS ); VERIFY_(STATUS)
-   call MAPL_GenericRunCouplers (STATE, I,        CLOCK,    RC=STATUS ); VERIFY_(STATUS)
+      I=CHEM
 
-   I=RADENV
+      call ESMF_GridCompRun (GCS(I), &
+         importState=GIM(I), exportState=GEX(I), clock=CLOCK, &
+         phase=1, userRC=STATUS); VERIFY_(STATUS)
+      call ESMF_GridCompRun (GCS(I), &
+         importState=GIM(I), exportState=GEX(I), clock=CLOCK, &
+         phase=2, userRC=STATUS); VERIFY_(STATUS)
+      call MAPL_GenericRunCouplers (STATE, I, CLOCK, _RC)
 
-   call ESMF_GridCompRun (GCS(I), importState=GIM(I), exportState=GEX(I), clock=CLOCK, userRC=STATUS ); VERIFY_(STATUS)
-   call MAPL_GenericRunCouplers (STATE, I,        CLOCK,    RC=STATUS ); VERIFY_(STATUS)
+      I=RADENV
 
-   I=RAD
+      call ESMF_GridCompRun (GCS(I), &
+         importState=GIM(I), exportState=GEX(I), clock=CLOCK, &
+         userRC=STATUS); VERIFY_(STATUS)
+      call MAPL_GenericRunCouplers (STATE, I, CLOCK, _RC)
 
-   call MAPL_TimerOn (STATE,GCNames(I))
-   call ESMF_GridCompRun (GCS(I), importState=GIM(I), exportState=GEX(I), clock=CLOCK, userRC=STATUS ); VERIFY_(STATUS)
-   call MAPL_GenericRunCouplers (STATE, I,        CLOCK,    RC=STATUS ); VERIFY_(STATUS)
-   call MAPL_TimerOff(STATE,GCNames(I))
+      I=RAD
 
-!  All done
-!  --------
-   RETURN_(ESMF_SUCCESS)
+      call MAPL_TimerOn (STATE, GCNames(I))
+      call ESMF_GridCompRun (GCS(I), &
+         importState=GIM(I), exportState=GEX(I), clock=CLOCK, &
+         userRC=STATUS ); VERIFY_(STATUS)
+      call MAPL_GenericRunCouplers (STATE, I, CLOCK, _RC)
+      call MAPL_TimerOff(STATE, GCNames(I))
+
+      RETURN_(ESMF_SUCCESS)
 
    END SUBROUTINE Run_
 
-   end module RootRad_GridCompMod
+end module RootRad_GridCompMod
