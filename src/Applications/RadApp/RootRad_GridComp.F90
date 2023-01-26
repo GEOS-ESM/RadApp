d23c4da6396a985b38e6a20dd07e268e5864f626
Peter Norris <peter.norris@nasa.gov>
2023-01-26 08:31:09 -0500

    tidy up code

--- src/Applications/RadApp/RootRad_GridComp.F90
+++ src/Applications/RadApp/RootRad_GridComp.F90
@@ -80,52 +80,60 @@ contains
       RADENV = MAPL_AddChild (GC, NAME='RADENV',    SS=RadiationEnvSetServices, _RC)
       RAD    = MAPL_AddChild (GC, NAME='RADIATION', SS=RadiationSetServices,    _RC)
 
-! Determine AERO and RATS provider
-! --------------------------------
+! Determine RATS (Radiatively Active Tracers) provider ...
+! For now PCHEM is the only option. It uses parameterized prediction from tabulated
+! zonally-symmetric production and loss data, or, alternatively, specification from
+! zonally-symmetric values. This data-based method avoids running the whole GCM. 
+! ---------------------------------------------------------------------------------
       call ESMF_ConfigGetAttribute (CF, &
          ratsName, Default="PCHEM", Label="RATS_PROVIDER:", _RC)
       if (trim(ratsName) == "PCHEM") then
          CHEM = MAPL_AddChild (GC, NAME='CHEMISTRY', SS=AChemSetServices, _RC)
+         call MAPL_AddConnectivity (GC, &
+            SHORT_NAME = [character(len=6):: 'OX','O3','CH4','N2O','CFC11','CFC12','HCFC22'], &
+            DST_ID     = RAD,     &
+            SRC_ID     = CHEM, _RC)
       end if
+      ! Technically, a non-PCHEM RATs Provider, say "NONE" would let these imports bubble up
+      ! and be filled by ExtData. However, the same approach is not trivial for getting the
+      ! AERO bundle below, so until changes are made to the AERO Provider section, PCHEM is
+      ! required below.
 
-! Imports of Radiation calculated in RadEnv not to come thru ExtData
+! Determine AERO provider
+! -----------------------
+      call ESMF_ConfigGetAttribute (CF, &
+         providerName, Label="AERO_PROVIDER:", Default="GOCART2G.data", _RC)
+      if (providerName == "GOCART2G.data") then
+         ! GOCARD AERO bundle comes through Chemistry GC
+         _ASSERT(trim(ratsName)=="PCHEM", "otherwise CHEM not defined above!")
+         aeroProvider = CHEM
+         ! terminate imports that are actually not required by GOCART2G.data
+         call MAPL_TerminateImport (GC, &
+            SHORT_NAME = [character(len=9):: 'FRLAND','FRLANDICE','FROCEAN','FRACI'],  &
+            CHILD = aeroProvider, &
+            RC = STATUS)
+      else
+         ! Perhaps we could devide a scheme where could examine import AERO bundle,
+         ! turn it into import FIELDs, get them from ExtData, and then compose the
+         ! AERO bundle. But the AERO bundle also has attached optical methods. Too
+         ! complicated. So rely on an established AERO_Provider. GOCAR2G.data is
+         ! data-driven and avoids running the whole GCM.
+         _FAIL("Unsupported Aerosol Provider!")
+      end if
+      call MAPL_AddConnectivity (GC,   &
+         SHORT_NAME = (/'AERO'/),      &
+         DST_ID     = RAD,             &
+         SRC_ID     = aeroProvider, _RC)
+
+! Imports of Radiation calculated in RadEnv rather than thru ExtData
 ! ------------------------------------------------------------------
       call MAPL_AddConnectivity (GC, &
          SHORT_NAME  = [character(len=4):: 'PREF','RI','RL'], &
          DST_ID      =  RAD,         &
          SRC_ID      =  RADENV,   _RC)
 
-      call ESMF_ConfigGetAttribute (CF, &
-         providerName, Label="AERO_PROVIDER:", Default="GOCART.data", _RC)
-
-!   if provider of aerobundle is pchem then we must terminate imports for its contents
-!?   so radenv doesn't expect them
-      if (providerName == "GOCART.data" .or. providerName == "GOCART") then
-         ASSERT_(trim(ratsName) == "PCHEM")
-         aeroProvider = CHEM
-         call MAPL_TerminateImport (GC, &
-            SHORT_NAME = [character(len=9):: 'FRLAND','FRLANDICE','FROCEAN','FRACI'],  &
-            CHILD = aeroProvider, &
-            RC = STATUS)
-      else
-         ASSERT_(.false.)
-      end if
-
-      call MAPL_AddConnectivity (GC,   &
-         SHORT_NAME = (/'AERO'/),      &
-         DST_ID     = RAD,             &
-         SRC_ID     = aeroProvider, _RC)
-
-!    for now pchem is the rats provider, no alternative
-!    --------------------------------------------------
-      if (trim(ratsName) == "PCHEM") then
-         call MAPL_AddConnectivity (GC, &
-            SHORT_NAME = [character(len=6):: 'OX','O3','CH4','N2O','CFC11','CFC12','HCFC22'], &
-            DST_ID     = RAD,           &
-            SRC_ID     = CHEM,       _RC)
-      end if
-
-!    Now have alternative Radiation coupling
+! Alternative Radiation coupling
+! ------------------------------
       call ESMF_ConfigGetAttribute (CF, &
          RadCouple, Default="USE_MODEL_RADCOUPLE_EXPORTS", Label="RADCOUPLE_METHOD:", _RC)
       if (trim(RadCouple) == "USE_MODEL_RADCOUPLE_EXPORTS") then
@@ -135,7 +143,7 @@ contains
          end if
       else if (trim(RadCouple) == "NEW_METHOD_1") then
          if (MAPL_AM_I_ROOT()) then
-            write(*,*) "Using new_method_1, QV,QL, QI, FCLD will be produced by RadEnv", &
+            write(*,*) "Using new_method_1: QV,QL,QI,FCLD will be produced by RadEnv", &
                "from Q,QILS,QICN, QLLS,QLCN,CLLS, and CLCN"
          end if
          call MAPL_AddConnectivity (GC, &
@@ -147,7 +155,6 @@ contains
 ! Generic Set Services
 ! --------------------
       call MAPL_GenericSetServices (GC, _RC)
-
       RETURN_(ESMF_SUCCESS)
 
    end subroutine SetServices
